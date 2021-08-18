ced64fe59c941d115e7fefad5a4214796f3339c7
devgeetech <devgeetech@gmail.com>
2021-08-19 04:55:37 +0530

    first commit

---
+++ components/utils/ClientOnly.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+type Props = {
+    children: any;
+    delegated?: any;
+}
+
+const ClientOnly: React.FC<Props> = ({ children, ...delegated }) => {
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) {
+    return null;
+  }
+
+  return <div {...delegated}>{children}</div>;
+}
+
+export default ClientOnly
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ApolloProvider } from "@apollo/client";
+import { UserProvider } from '@auth0/nextjs-auth0';
+import client from "../apollo-client";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider>
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </UserProvider>
+  );
 }
 export default MyApp
