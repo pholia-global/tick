9c79fff8bd024b07c47ec90ea0bd486e5c2f9a0e
devgeetech <devgeetech@gmail.com>
2021-09-25 10:40:09 +0530

    husky 3

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  testPathIgnorePatterns: [
+    "<rootDir>/.next/",
+    "<rootDir>/node_modules/",
+    "<rootDir>/cypress/",
+  ],
   moduleNameMapper: {
     "\\.(scss|sass|css)$": "identity-obj-proxy",
     "^@/components/(.*)$": ["<rootDir>/src/components/$1"],
